739d5ffc088924ba0f272196843dddb90619300a
Maxim <zinoviev.maxim@vk.com>
2025-06-01 21:16:09 +0300

    remove ssl

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import ssl
 
 app = Flask(__name__)
 
@@ -9,6 +8,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain(certfile='ssl/certificate_ca.crt', keyfile='ssl/certificate.key')
-    app.run(host="0.0.0.0", debug=True, port=5000, ssl_context=context)
+    app.run(host="0.0.0.0", debug=True, port=5000)
